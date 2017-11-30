df614ed8c595d05bf60f6abf7636094ee547eb35
胡文哲 <Wenzhe.hu@da-mai.com>
2017-11-30 20:28:49 +0800

    Site updated: 2017-11-30 20:28:49

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://huwenzhe.com/Blog-Back-Up/min_photos/' + data.link[i];
-          var src = 'http://huwenzhe.com/Blog-Back-Up/photos/' + data.link[i];
+          var minSrc = 'http://huwenzhe.com/Blog-Back-Up/photos/' + data.link[i];
+          var src = 'http://huwenzhe.com/Blog-Back-Up/min_photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
@@ -342,6 +342,7 @@
         var thumbElements = el.getElementsByClassName('thumb'),
           numNodes = thumbElements.length,
           items = [],
+          figureEl,
           figureEl,
           linkEl,
           size,
